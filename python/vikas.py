c6bca715e8c3fa1b7b7109e6f9207294b7e0c1c7
krishnavikas <krishnavikas@ccs.neu.edu>
2016-04-15 05:08:15 -0400

    First Commit

---
+++ python/vikas.py
@@ -0,0 +1,5 @@
+import nltk as nt
+import pandas as pd
+import matplotlib as mt
+
+print  "Hello World"
---
+++ shell scripts/vikas.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This program displays all global variables in shel script
+
+echo "user $USER"
+
+echo "
