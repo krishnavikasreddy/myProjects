df9721b234c744e0fc72817e3cf6d2a99b59e743
krishna <krishna.vikasreddy@gmail.com>
2017-06-29 21:47:00 -0400

    some changes

--- images/google-images.sh
+++ images/google-images.sh
@@ -2,7 +2,7 @@
 dir_spaceless=`echo $1 | tr " " - `
 save_path="$HOME/Music/1/.1/$dir_spaceless/"
 echo "Default save path is $save_path"
-keyword=`echo $1 | sed 's/ /%20/g'` 
+keyword=`echo $1 | sed 's/ /%20/g'`
 google_url="https://www.google.com/search?site=&tbm=isch&q=$keyword"
 
 echo $save_path
@@ -16,7 +16,5 @@ if [[ -d "$save_path" ]]; then
     exit;
 else
     mkdir -p $save_path
-    wget -q -O- --header="$header" --user-agent="$user_agent" $google_url | grep -oP '<div class=\"rg_meta\">(.*?)\}' |grep -oP "(\w+)\:(\/{2})(.*?)(.jpg|.png)" | wget -t1 --connect-timeout=4 -i - -P "$save_path"
+    wget -q -O- --header="$header" --user-agent="$user_agent" $google_url | grep -oP '<div class=\"rg_meta(.*?)\">(.*?)\}' |grep -oP "(\w+)\:(\/{2})(.*?)(.jpg|.png)" | wget -t1 --connect-timeout=4 -i - -P "$save_path"
 fi
-
-
